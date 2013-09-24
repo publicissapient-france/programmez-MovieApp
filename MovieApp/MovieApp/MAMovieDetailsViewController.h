4a5146dceb12401185e01618cf4767ebd1b6bcf3
Martin Moizard <mmoizard@xebia.fr>
2013-09-24 10:55:02 +0200

    Storyboard enhanced

---
+++ MovieApp/MovieApp/MAMovieDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MAMovieDetailsViewController.h
+//  MovieApp
+//
+//  Created by Martin Moizard on 24/09/13.
+//  Copyright (c) 2013 Xebia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAMovieDetailsViewController : UIViewController
+
+@end
